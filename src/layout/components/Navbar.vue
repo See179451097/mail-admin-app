8aa3ab6dc7bc42f569f72a57489ada44cbfcf82d
See179451097 <10360352+see179451097@user.noreply.gitee.com>
2022-03-11 12:37:16 +0800

    系统初始布局

---
+++ src/layout/components/Navbar.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="navbar">
+    <hamburger
+      :is-active="sidebar.opened"
+      class="hamburger-container"
+      @toggleClick="toggleSideBar"
+    />
+
+    <breadcrumb class="breadcrumb-container" />
+
+    
+    <el-dropdown class="nav-right">
+      <span class="el-dropdown-link">
+        {{ this.name }}<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>狮子头</el-dropdown-item>
+        <el-dropdown-item @click.native="logout" divided>退出登录</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Breadcrumb from "@/components/Breadcrumb";
+import Hamburger from "@/components/Hamburger";
+
+export default {
+  components: {
+    Breadcrumb,
+    Hamburger,
+  },
+  computed: {
+    ...mapGetters(["sidebar", "avatar", "name"]),
+  },
+  methods: {
+    toggleSideBar() {
+      this.$store.dispatch("app/toggleSideBar");
+    },
+    async logout() {
+      await this.$store.dispatch("user/logout");
+      this.$router.push(`/login?redirect=${this.$route.fullPath}`);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+
+  .hamburger-container {
+    line-height: 46px;
+    height: 100%;
+    float: left;
+    cursor: pointer;
+    transition: background 0.3s;
+    -webkit-tap-highlight-color: transparent;
+
+    &:hover {
+      background: rgba(0, 0, 0, 0.025);
+    }
+  }
+
+  .breadcrumb-container {
+    float: left;
+  }
+  .nav-right {
+    position: absolute;
+    right: 20px;
+    top: 0;
+    line-height: 50px;
+  }
+
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #555;
+  }
+  .el-icon-arrow-down {
+    font-size: 16px;
+    font-weight: 600;
+  }
+}
+</style>
---
+++ src/views/dataStatistics/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="data-statistics-container">
+    <div class="card-container">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>商品总览</span>
+          <el-button style="float: right; padding: 3px 0" type="text"
+            >操作按钮</el-button
+          >
+        </div>
+        <div class="text item">content</div>
+      </el-card>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>用户总览</span>
+          <el-button style="float: right; padding: 3px 0" type="text"
+            >操作按钮</el-button
+          >
+        </div>
+        <div class="text item">content</div>
+      </el-card>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>销售总览</span>
+          <el-button style="float: right; padding: 3px 0" type="text"
+            >操作按钮</el-button
+          >
+        </div>
+        <div class="text item">content</div>
+      </el-card>
+    </div>
+    <div class="vline-container">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style scoped>
+.card-container {
+  margin: 15px;
+  display: flex;
+  justify-content: space-between;
+  flex: 1 1 auto;
+}
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  width: 100%;
+}
+.box-card:nth-child(2) {
+  margin: 0 15px;
+}
+</style>
